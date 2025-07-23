f162c596806e4bc396895a587d7979d21a949627
Clement_127001 <babiyonclement18@gmail.com>
2025-07-23 10:43:56 +0530

    fixed env issue

--- Scrim-s05ll6h/index.js
+++ Scrim-s05ll6h/index.js
@@ -1,14 +1,21 @@
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { createClient } from "@supabase/supabase-js";
+
 import scrimbaInfo from "./utils/scrimba-info.js";
 
 try {
-  try {
-    const splitter = new RecursiveCharacterTextSplitter();
-    const output = await splitter.createDocuments([scrimbaInfo]);
-    console.log(output);
-  } catch (err) {
-    console.log("Error occurs : " + err.message);
-  }
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 500,
+    chunkOverlap: 50,
+    separators: ["\n\n", "\n", " ", ""],
+  });
+  const output = await splitter.createDocuments([scrimbaInfo]);
+
+  const sbApiKey = import.meta.env.VITE_SUPABASE_API_KEY;
+  const sbUrl = import.meta.env.VITE_SUPABASE_URL_LC_CHATBOT;
+  const openAIApiKey = import.meta.env.VITE_OPENAI_API_KEY;
+
+  const client = createClient(sbUrl, sbApiKey);
 } catch (err) {
-  console.log(err);
+  console.log("Error occurs : " + err.message);
 }
