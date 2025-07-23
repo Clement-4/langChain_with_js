9293ab13eaeb57932d1e974618c14fd8758c2489
Clement_127001 <babiyonclement18@gmail.com>
2025-07-23 10:19:07 +0530

    fixed the file loading issue

--- Scrim-s05ll6h/index.js
+++ Scrim-s05ll6h/index.js
@@ -1,6 +1,14 @@
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import scrimbaInfo from "./utils/scrimba-info.js";
+
 try {
-  const result = await fetch('scrimba-info.txt')
-  const text = await result.text()
+  try {
+    const splitter = new RecursiveCharacterTextSplitter();
+    const output = await splitter.createDocuments([scrimbaInfo]);
+    console.log(output);
+  } catch (err) {
+    console.log("Error occurs : " + err.message);
+  }
 } catch (err) {
-  console.log(err)
+  console.log(err);
 }
