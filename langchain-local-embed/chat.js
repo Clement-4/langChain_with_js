c83b09962745b4edd8da51fddffb62fe56fe3872
clement-4 <babiyon.clement@hippoinnovations.com>
2025-07-24 12:50:55 +0530

    add invoker for store

--- langchain-local-embed/chat.js
+++ langchain-local-embed/chat.js
@@ -1,5 +1,9 @@
+import "dotenv/config";
 import { PromptTemplate } from "@langchain/core/prompts";
 import { Ollama } from "@langchain/ollama";
+import { OllamaEmbeddings } from "@langchain/community/embeddings/ollama";
+import { SupabaseVectorStore } from "@langchain/community/vectorstores/supabase";
+import EnhancedSupabaseVectorStore from "./enhancedSupabaseVectorStore.js";
 
 const llm = new Ollama({
   host: "http://localhost:11434",
@@ -7,6 +11,19 @@ const llm = new Ollama({
   temperature: 0,
   maxRetries: 2,
 });
+const store = new EnhancedSupabaseVectorStore();
+
+const embeddings = new OllamaEmbeddings({
+  model: "nomic-embed-text",
+  baseUrl: "http://localhost:11434",
+});
+
+const vectorStore = new SupabaseVectorStore(embeddings, {
+  client: store.getClient(),
+  tableName: "documents",
+  queryName: "match_documents",
+});
+const retriever = vectorStore.asRetriever();
 
 const standaloneQuestionTemplate =
   "Given a question, convert it to a standalone question. question: {question} standalone question";
@@ -17,13 +34,11 @@ const standaloneQuestionPrompt = PromptTemplate.fromTemplate(
 
 const standaloneQuestionChain = standaloneQuestionPrompt.pipe(llm);
 
-const response = await standaloneQuestionChain.invoke({
+const standAloneQuestion = await standaloneQuestionChain.invoke({
   question:
-    "What are the technical requirements for running Scrimba? I think of doing something cool with scrimba",
+    "What are the technical requirements for running Scrimba? I only have a very old laptop which is not that powerful.",
 });
 
+const response = await retriever.invoke(standAloneQuestion);
+
 console.log(response);
-
-// const tweetPrompt = PromptTemplate.fromTemplate(tweetTemplate);
-
-// console.log(tweetPrompt);
